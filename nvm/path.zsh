5a41b17dac5844919dd687344523e9bae257ddaf
James Diefenderfer <jimmydief@gmail.com>
2018-03-20 13:28:20 -0700

    add nvm settings

---
+++ nvm/path.zsh
@@ -0,0 +1,7 @@
+export NVM_DIR="$HOME/.nvm"
+
+# load nvm
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# load nvm bash_completion
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
