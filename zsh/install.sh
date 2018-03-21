17b0ac08fa98aa3b7166cc3e0ef55a8bf9f6eb44
James Diefenderfer <jimmydief@gmail.com>
2018-03-21 10:36:21 +0500

    conditionally install all ZSH dependencies

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,17 +1,51 @@
 #!/bin/sh
 #
-# ZSH Autosuggestions
+# ZSH
 #
-# This installs autosuggestions for ZSH.
 
+# Install ZSH.
+if test ! $(which brew)
+then
+	echo -e "Installing ZSH."
+
+	brew install zsh
+else
+	echo -e "ZSH is already installed, skipping..."
+fi
+
+# Ensure that ZSH is a valid shell in the eyes of the system.
+if grep -q "/usr/local/bin/zsh" /etc/shells; then
+	echo -e "ZSH is already a valid shell, skipping..."
+else
+	echo -e "Adding ZSH to /etc/shells."
+
+	sudo bash -c 'echo "/usr/local/bin/zsh" >> /etc/shells'
+fi
+
+if [ $SHELL = "/bin/zsh" ]; then
+	echo -e "ZSH is already the default shell, skipping..."
+else
+	echo -e "Making ZSH the default shell."
+
+	chsh -s /usr/local/bin/zsh
+fi
+
+# Install oh-my-zsh if necessary.
+if [ -d $ZSH ]; then
+	echo -e "$ZSH folder already exists, skipping install of oh-my-zsh..."
+else
+	echo -e "Installing oh-my-zsh."
+
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+# Install autosuggestions for ZSH if necessary.
 if [ -d "$ZSH/custom/plugins/zsh-autosuggestions" ]; then
-		echo -e "zsh-suggestions already installed, skipping..."
-	else
-		echo -e "Installing zsh-suggestions."
+	echo -e "zsh-suggestions already installed, skipping..."
+else
+	echo -e "Installing zsh-suggestions."
 
-		# clone the autosuggestions repository if it doesn't already exist
-		git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH/custom/plugins/zsh-autosuggestions
-
+	git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH/custom/plugins/zsh-autosuggestions
 fi
 
 exit 0
