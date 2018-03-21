a8f10a937eff7b56bbba467a427bd4dab78843cb
James Diefenderfer <jimmydief@gmail.com>
2018-03-20 22:10:20 -0700

    add sublime configs

---
+++ sublime/install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Set up a machine for Sublime Text 3
+
+set -x
+
+sublime_dir=~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+
+# symlink an "st" command to open files in Sublime Text from the command line.
+ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/st
+
+if [ -d "$sublime_dir" ]; then
+	echo -e "Sublime Text 3 /User folder already exists."
+else
+	echo -e "Creating Sublime Text 3 /User folder."
+
+	mkdir -p "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+fi
+
+# symlink user settings
+for file in $ZSH/sublime/User/*
+do
+	echo -e "Symlinking $file."
+	ln -sf "$file" "$sublime_dir"
+done
